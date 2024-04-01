5ce1a98af5e3c0fc0bf3d4c476496349e5dd57b2
Kristina <kr.hranovska@gmail.com>
2024-04-01 17:50:33 +0300

    Update 3.tsx

--- src/homework/3.tsx
+++ src/homework/3.tsx
@@ -1,13 +1,14 @@
-// import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
-// export function FormComponent() {
-//   const [value, setValue] = useState("");
+export function FormComponent() {
+    const [value, setValue] = useState<string>("");
 
-//   const handleChange = (event) => {
-//     setValue(event.target.value);
-//   };
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value);
+        console.log('Введений текст:', event.target.value);
+    };
 
-//   return (
-//     <input type="text" value={value} onChange={handleChange} />
-//   );
-// }
+    return (
+        <input type="text" value={value} onChange={handleChange} />
+    );
+}
